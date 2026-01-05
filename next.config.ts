777b4550e14819201d99ad1daaea7ec0a61f9492
smarthub43 <smartearninghub43@gmail.com>
2026-01-05 20:27:02 +0530

    Fix: Ignore canvas in next.config.ts for vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // This tells Next.js to ignore the 'canvas' module during build
+    config.resolve.alias.canvas = false;
+    
+    return config;
+  },
 };
 
 export default nextConfig;
