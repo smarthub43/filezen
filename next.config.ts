acbc5434d6d9806e0eb6c63c13f7ad6a2ae709a1
smarthub43 <smartearninghub43@gmail.com>
2026-01-05 20:40:39 +0530

    Fix build: Remove conflicting webpack config and pdfjs-dist

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: any = {
-  // We don't need externalPackages anymore because we aren't using the npm package!
-  
-  // Keep these just to be safe and save memory
+  // 1. Prevent Vercel from trying to bundle your other PDF library
+  serverExternalPackages: ["pdf-lib"],
+
+  // 2. Disable checks to save memory
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  
-  // Basic webpack config just in case
-  webpack: (config: any) => {
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
-    return config;
   },
 };
 
